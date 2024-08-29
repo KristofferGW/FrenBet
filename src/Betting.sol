75dcd50e90b1be909f964d1d5092b2b6db79def8
KristofferGW <kristoffer.wallqvist@medieinstitutet.se>
2024-08-29 23:02:29 +0200

    More work on settle bet logic

--- src/Betting.sol
+++ src/Betting.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.19;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {FunctionsConsumer} from "./FunctionsConsumer.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Betting {
+    FunctionsConsumer public functionsConsumer;
+
     struct Bet {
         uint256 matchId;
         string predictedOutcome;
@@ -30,6 +34,7 @@ contract Betting {
     mapping(uint256 => BetSlip) public betSlips; // Mapping to store bet slips by their unique ID
     mapping(uint256 => uint256[]) public betSlipsByGroup; // Mapping to store bet slips by groupId
     mapping(uint256 => Group) public groups; // Mapping to store groups by their unique ID
+    mapping(uint256 => string) public matchResults; // Mapping match results to match ID
     mapping(address => uint256[]) public userBetSlips; // Mapping to store bet slip IDs by user address
     IERC20 public usdcToken; // USDC token contract
 
@@ -37,8 +42,9 @@ contract Betting {
     event GroupSettled(uint256 indexed groupId);
 
     // Constructor to set the USDC token contract address
-    constructor(address _usdcTokenAddress) {
+    constructor(address _usdcTokenAddress, address _functionsConsumerAddress) {
         usdcToken = IERC20(_usdcTokenAddress);
+        functionsConsumer = FunctionsConsumer(_functionsConsumerAddress);
     }
 
     // Function to create a new bet slip and associate it with a group
@@ -98,16 +104,41 @@ contract Betting {
         return groups[groupId];
     }
 
-    function settleGroup(uint256 groupId) public {
-        // check that the last game of the groups bet slip has been played
+    function requestMatchResult(uint256 matchId) internal {
+        string memory source = "<Your Javascript Code>"; // How can I import this?
+        bytes memory encryptedSecretUrls = "<Your Encrypted Secrets>"; // How can I import this?
+        uint8 donHostedSecretsSlotId = 0; // Use the appropriate value, do I even need it when using gist?
+        uint64 donHostedSecretsVersion = 0; // Use the appropriate value, do I even need it when using gist?
+        string[] memory args;
+        args[0] = Strings.toString(matchId); 
+        bytes[] memory bytesArgs; // Empty if you don't have any bytesArgs
+        uint64 subscriptionId = 1234;
+        uint32 gasLimit = 300000;
+        bytes32 donId = keccak256(abi.encodePacked("fun-ethereum-sepolia-1"));
 
-        // find every bet slip that corresponds to the group
+        bytes32 requestId = functionsConsumer.sendRequest(
+            source,
+            encryptedSecretUrls,
+            donHostedSecretsSlotId,
+            donHostedSecretsVersion,
+            args,
+            bytesArgs,
+            subscriptionId,
+            gasLimit,
+            donId
+        );
+    }
+
+    function settleGroup(uint256 groupId) public {
         uint256[] memory betSlipIds = betSlipsByGroup[groupId];
 
-        // fetch the results of each match in the bet slip using chainlink
+        for (uint256 i = 0; i < betSlipIds.bets.length; i++) {
+            uint256 matchId = betSlipIds[i];
+
+            requestMatchResult(matchId);
+        }
 
         // calculate scores
-
         // distribute winnings
     }
 }
