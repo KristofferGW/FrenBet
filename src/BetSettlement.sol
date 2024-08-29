0b48e542c39fbace827c4e8334e3aa12125024de
KristofferGW <kristoffer.wallqvist@medieinstitutet.se>
2024-08-29 17:25:08 +0200

    Started settleGroup function

--- src/BetSettlement.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.19;
-
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./Betting.sol";
-
-contract BetSettlement {
-    
-}
--- src/Betting.sol
+++ src/Betting.sol
@@ -19,13 +19,16 @@ contract Betting {
 
     struct Group {
         uint256 balance;
+        BetSlip[] betSlips;
         uint256 groupId;
         bool settled;
     }
 
+    uint256 constant BET_COST = 10; 
     uint256 public betSlipCounter;
     uint256 public groupCounter;
     mapping(uint256 => BetSlip) public betSlips; // Mapping to store bet slips by their unique ID
+    mapping(uint256 => uint256[]) public betSlipsByGroup; // Mapping to store bet slips by groupId
     mapping(uint256 => Group) public groups; // Mapping to store groups by their unique ID
     mapping(address => uint256[]) public userBetSlips; // Mapping to store bet slip IDs by user address
     IERC20 public usdcToken; // USDC token contract
@@ -54,7 +57,7 @@ contract Betting {
         newBetSlip.groupId = groupId;
 
         // Populate the bets array in the BetSlip struct
-        for (uint256 i; i < matchIds.length; i++) { // is it cheaper not look up the length every time even though it's from memory?
+        for (uint256 i; i < matchIds.length; i++) {
             newBetSlip.bets.push(Bet({
                 matchId: matchIds[i],
                 predictedOutcome: predictedOutcomes[i]
@@ -63,12 +66,12 @@ contract Betting {
 
         // Store the bet slip ID in the user's record and the group
         userBetSlips[msg.sender].push(betSlipCounter);
-        groups[groupId].betSlips.push(betSlipCounter);
+        betSlipsByGroup[groupId].push(betSlipCounter);
 
         // Update the group's total bet amount
-        groups[groupId].totalBetAmount += BET_COST;
+        groups[groupId].balance += BET_COST;
 
-        emit BetSlipCreated(msg.sender, betSlipCounter, groupId);
+        emit BetSlipCreated(msg.sender, betSlipCounter);
 
         betSlipCounter++;
     }
@@ -77,7 +80,7 @@ contract Betting {
         Group storage newGroup = groups[groupCounter];
         newGroup.groupId = groupCounter;
         newGroup.settled = false;
-        return newGroup;
+        groupCounter++;
     }
 
     // Function to get bet slips of a user
@@ -93,5 +96,18 @@ contract Betting {
     // Function to get details of a specific group
     function getGroupDetails(uint256 groupId) public view returns (Group memory) {
         return groups[groupId];
+    }
+
+    function settleGroup(uint256 groupId) public {
+        // check that the last game of the groups bet slip has been played
+
+        // find every bet slip that corresponds to the group
+        uint256[] memory betSlipIds = betSlipsByGroup[groupId];
+
+        // fetch the results of each match in the bet slip using chainlink
+
+        // calculate scores
+
+        // distribute winnings
     }
 }
